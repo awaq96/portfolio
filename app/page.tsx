6208acb9ee2cf52ee11e8811610fbda0099830eb
Asher Waqar <awawar6@gatech.edu>
2026-04-25 15:53:03 -0700

    Move GT Projects above Side

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
     <main>
       <Intro />
       <EnterpriseSection />
-      <SideProjectsSection />
       <GTProjectsSection />
+      <SideProjectsSection />
     </main>
   );
 }
